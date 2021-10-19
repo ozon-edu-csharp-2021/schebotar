f5544f8000d37d22dcc1ba62a751c290dbdbeca8
Sergey Chebotar <s.chebotar@gmail.com>
2021-10-19 09:46:29 +0300

    Add dockerfile and docker-compose

--- src/OzonEdu.MerchApi/Startup.cs
+++ src/OzonEdu.MerchApi/Startup.cs
@@ -10,29 +10,19 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 
 namespace OzonEdu.MerchApi
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseRouting();
-            app.UseEndpoints(endpoints => {  });
+            app.UseEndpoints(endpoints => { });
         }
     }
 }
